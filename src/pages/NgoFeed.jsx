cdaf2fe9f327148fd709cc656be3ecc0bc97cdf4
Aleix Abulí <aleixabuli088@gmail.com>
2022-03-12 21:41:52 +0100

    started form for filtering hosts

--- src/pages/NgoFeed.jsx
+++ src/pages/NgoFeed.jsx
@@ -13,10 +13,45 @@ export default function NgoFeed(){
         .catch(err => console.log(err));
     }, []);
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        
+    };
+
     return(
         <div>
-            <h1>Browse available accommodations:</h1>
             <div>
+                <h1>Filter hosting:</h1>
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="input-firstName">Rooms:</label>
+                    <input
+                        id="input-rooms"
+                        type="number"
+                        name="rooms"
+                        placeholder="Number of rooms"
+                        onChange={''}
+                        required
+                    />
+
+                    <label htmlFor="input-firstName">Capacity:</label>
+                    <input
+                        id="input-capacity"
+                        type="number"
+                        name="capacity"
+                        placeholder="The total of people you can host"
+                        onChange={''}
+                        required
+                    />
+
+                    <button className="button__submit" type="submit">
+                        Filter
+                    </button>
+
+                </form>
+            </div>
+            <div>
+            <h1>Browse available accommodations:</h1>
                 {accommodations.map((accommodation) => (
                     <AccommodationCard key={accommodation._id} accommodation={accommodation} />
                 ))}
