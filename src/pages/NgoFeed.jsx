02d5cc53f2ff20dca6a6d3a25863ef53a380bb25
Aleix Abulí <aleixabuli088@gmail.com>
2022-03-13 11:35:30 +0100

    filter accommodations working

--- src/pages/NgoFeed.jsx
+++ src/pages/NgoFeed.jsx
@@ -5,18 +5,38 @@ import AccommodationCard from "../components/AccommodationCard";
 export default function NgoFeed(){
 
     const [accommodations, setAccommodations] = useState([]);
+    const [rooms, setRooms] = useState(0);
+    const [capacity, setCapacity] = useState(0);
+    const [errorMessage, setErrorMessage] = useState(null);
+    const [accomFromDB, setAccomFromDB] = useState([]);
 
     useEffect(()=>{
         axios
         .get(`${process.env.REACT_APP_SERVER_URL}/accommodations`)
-        .then(response => setAccommodations(response.data))
+        .then(response => {
+            setAccomFromDB(response.data)
+            setAccommodations(response.data)
+        })
         .catch(err => console.log(err));
     }, []);
+
+    const handleRooms = (e) => setRooms(e.target.value);
+    const handleCapacity = (e) => setCapacity(e.target.value);
 
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        
+        let results = accommodations.filter((accommodation) => {
+            return (accommodation.capacity === parseFloat(capacity) && accommodation.rooms === parseFloat(rooms));
+        });
+
+        if (results.length === 0) {
+            setErrorMessage("Sorry, there wasn't any match for you search.");
+            setAccommodations(accomFromDB);
+        } else {
+            setAccommodations(results);
+            setErrorMessage(null);
+        };
     };
 
     return(
@@ -30,7 +50,8 @@ export default function NgoFeed(){
                         type="number"
                         name="rooms"
                         placeholder="Number of rooms"
-                        onChange={''}
+                        value={rooms}
+                        onChange={handleRooms}
                         required
                     />
 
@@ -39,8 +60,9 @@ export default function NgoFeed(){
                         id="input-capacity"
                         type="number"
                         name="capacity"
-                        placeholder="The total of people you can host"
-                        onChange={''}
+                        placeholder="Number of people you need host"
+                        value={capacity}
+                        onChange={handleCapacity}
                         required
                     />
 
@@ -49,6 +71,10 @@ export default function NgoFeed(){
                     </button>
 
                 </form>
+
+                {errorMessage && (
+                    <p>{errorMessage}</p>
+                )}
             </div>
             <div>
             <h1>Browse available accommodations:</h1>
