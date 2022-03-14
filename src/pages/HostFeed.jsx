381b298750efd595225a8731e06252cf3cbc75a2
Aleix Abulí <aleixabuli088@gmail.com>
2022-03-14 13:37:07 +0100

    request hosting only appears if you are an ngo

--- src/pages/HostFeed.jsx
+++ src/pages/HostFeed.jsx
@@ -5,16 +5,11 @@ import NgoCard from "../components/NgoCard";
 export default function HostFeed(){
 
     const [ngos, setNgos] = useState([]);
-    const [rooms, setRooms] = useState(0);
-    const [capacity, setCapacity] = useState(0);
-    const [errorMessage, setErrorMessage] = useState(null);
-    const [ngosFromDB, setNgosFromDB] = useState([]);
 
     useEffect(()=>{
         axios
         .get(`${process.env.REACT_APP_SERVER_URL}/ngo`)
         .then(response => {
-            setNgosFromDB(response.data)
             setNgos(response.data)
         })
         .catch(err => console.log(err));
