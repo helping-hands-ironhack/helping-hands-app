1f4f4cdaba4e0a380e203d94c9ad749685ef9c8f
Aleix Abulí <aleixabuli088@gmail.com>
2022-03-14 13:27:58 +0100

    filter de hostFeed eliminado

--- src/pages/HostFeed.jsx
+++ src/pages/HostFeed.jsx
@@ -20,68 +20,12 @@ export default function HostFeed(){
         .catch(err => console.log(err));
     }, []);
 
-    const handleRooms = (e) => setRooms(e.target.value);
-    const handleCapacity = (e) => setCapacity(e.target.value);
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-
-        let results = ngos.filter((accommodation) => {
-            return (accommodation.capacity === parseFloat(capacity) && accommodation.rooms === parseFloat(rooms));
-        });
-
-        if (results.length === 0) {
-            setErrorMessage("Sorry, there wasn't any match for you search.");
-            setNgos(ngosFromDB);
-        } else {
-            setNgos(results);
-            setErrorMessage(null);
-        };
-    };
-
     return(
         <div>
-            <div>
-                <h1>Filter NGOs:</h1>
-                <form onSubmit={handleSubmit}>
-                    <label htmlFor="input-firstName">Rooms:</label>
-                    <input
-                        id="input-rooms"
-                        type="number"
-                        name="rooms"
-                        placeholder="Number of rooms"
-                        value={rooms}
-                        onChange={handleRooms}
-                        required
-                    />
-
-                    <label htmlFor="input-firstName">Capacity:</label>
-                    <input
-                        id="input-capacity"
-                        type="number"
-                        name="capacity"
-                        placeholder="Number of people you need host"
-                        value={capacity}
-                        onChange={handleCapacity}
-                        required
-                    />
-
-                    <button className="button__submit" type="submit">
-                        Filter
-                    </button>
-
-                </form>
-
-                {errorMessage && (
-                    <p>{errorMessage}</p>
-                )}
-            </div>
-            <div>
             <h1>Browse all registered NGOs:</h1>
-                {ngos.map((ngo) => (
-                    <NgoCard key={ngo._id} ngo={ngo} />
-                ))}
-            </div>
+            {ngos.map((ngo) => (
+                <NgoCard key={ngo._id} ngo={ngo} />
+            ))}
         </div>
     );
 };
