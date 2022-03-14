25ef4401dcf8cb8e7d19440e4d21d9b2e3eda4a3
Marcelus <kz_@hotmail.es>
2022-03-14 19:55:43 +0100

    Requests working

--- src/pages/HostFeed.jsx
+++ src/pages/HostFeed.jsx
@@ -1,22 +1,26 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import NgoCard from "../components/NgoCard";
+import RequestsForHost from "../components/RequestsForHost";
+import { AuthContext } from "../context/auth.context";
 
-export default function HostFeed(){
+export default function HostFeed() {
 
     const [ngos, setNgos] = useState([]);
+    
 
-    useEffect(()=>{
+    useEffect(() => {
         axios
-        .get(`${process.env.REACT_APP_SERVER_URL}/ngo`)
-        .then(response => {
-            setNgos(response.data)
-        })
-        .catch(err => console.log(err));
+            .get(`${process.env.REACT_APP_SERVER_URL}/ngo`)
+            .then(response => {
+                setNgos(response.data)
+            })
+            .catch(err => console.log(err));
     }, []);
 
-    return(
+    return (
         <div>
+            <RequestsForHost />
             <h1>Browse all registered NGOs:</h1>
             {ngos.map((ngo) => (
                 <NgoCard key={ngo._id} ngo={ngo} />
