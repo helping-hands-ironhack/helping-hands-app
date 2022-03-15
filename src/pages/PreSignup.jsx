97072e63e403f14bd0192ad346aae77113e5b9db
Aleix Abulí <aleixabuli088@gmail.com>
2022-03-15 18:35:19 +0100

    pre signup css done

--- src/pages/PreSignup.jsx
+++ src/pages/PreSignup.jsx
@@ -4,15 +4,19 @@ import './auth.css';
 export default function PreSignup(){
     return(
         <div className="formBackground">
-            <h1>What are you?</h1>
-            <h3>Choose the type of user you want to register: </h3>
-            <Link to='/auth/host/signup'>
-                <button>Host</button>
-            </Link>
-            
-            <Link to='/auth/ngo/signup'>
-                <button>NGO</button>
-            </Link>
+            <div className="formContainer">
+                <h1>What are you?</h1>
+                <h3>Choose the type of user you want to register: </h3>
+                <div className="choiceBtns">
+                    <Link to='/auth/host/signup' className="choiceSingleBtn">
+                        Host
+                    </Link>
+                
+                    <Link to='/auth/ngo/signup' className="choiceSingleBtn">
+                        NGO
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 };
