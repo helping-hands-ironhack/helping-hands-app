6f31e66c469fa820cdbb57cde8c5411d1a48b9cd
Aleix Abulí <aleixabuli088@gmail.com>
2022-03-15 18:10:53 +0100

    check commit

--- src/pages/PreSignup.jsx
+++ src/pages/PreSignup.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
+import './auth.css';
 
 export default function PreSignup(){
     return(
-        <div>
+        <div className="formBackground">
             <h1>What are you?</h1>
-            <p>Choose the type of user you want to create: </p>
+            <h3>Choose the type of user you want to register: </h3>
             <Link to='/auth/host/signup'>
                 <button>Host</button>
             </Link>
