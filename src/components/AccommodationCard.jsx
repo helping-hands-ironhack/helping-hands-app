8500e239a12bf70f57c415b512c5d4c0a72e7d45
Stan T <86654387+Twalinzor@users.noreply.github.com>
2022-03-16 12:37:29 +0100

    Some ctx stuff

--- src/components/AccommodationCard.jsx
+++ src/components/AccommodationCard.jsx
@@ -1,12 +1,26 @@
+import axios from "axios";
 import { Link } from "react-router-dom";
+import { useContext  } from "react";
+import { AuthContext } from "../context/auth.context";
 
 export default function AccommodationCard({accommodation}){
+    const {user} = useContext(AuthContext);
+
+    const isOwner = accommodation.owner === user._id;
+    // console.log("THEM PROPS ==========>", props)
+  //  axios
+        // .get(`${process.env.REACT_APP_SERVER_URL}/users/${id}`)
+
     return(
-        <Link to={`/accommodation/${accommodation._id}`}>
-            <div>
+        
+        <div>
+        <Link to={`/users/${accommodation.owner._id}`}>Host owner o alguna mierda</Link>    
+        <div>
+            <Link to={`/accommodation/${accommodation._id}`}>
                 <h2>{accommodation.description}</h2>
                 <img src={accommodation.imageUrl} alt=''/>
-            </div>
-        </Link>
+            </Link>                
+        </div>
+        </div>
     );
 };
--- src/components/AccommodationsFeed.jsx
+++ src/components/AccommodationsFeed.jsx
@@ -1,15 +1,21 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../context/auth.context";
 
 export default function AccommodationsFeed(props) {
     const accommodations = props.user.accommodations
 
+    const {user} = useContext(AuthContext);
+    const isOwner = user._id === props.user._id;
+
     return (
         <div>
             <h1>These are your Accommodations</h1>
-            <Link to={`/users/${props.user._id}/accommodations/create`}>Add a new Accommodation</Link>
+            {isOwner && <Link to={`/users/${props.user._id}/accommodations/create`}>Add a new Accommodation</Link>}
             <div className="acccommodations-container">
                 {accommodations && (
                     accommodations.map((acc) => {
+
                         return(
                             <Link key={acc._id} to={`/accommodation/${acc._id}`}>
                                 <img src={acc.imageUrl} alt="" />
